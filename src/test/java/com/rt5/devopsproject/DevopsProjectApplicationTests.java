83833f48a3acb582d48643d4552e33a3ffca0f93
Eya_cfu <zaouieya@gmail.com>
2022-12-02 14:38:54 +0100

    first commit

---
+++ src/test/java/com/rt5/devopsproject/DevopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rt5.devopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
